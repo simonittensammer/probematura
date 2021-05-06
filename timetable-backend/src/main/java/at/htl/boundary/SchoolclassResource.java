e92e31cdc58bf9e04bb507d6e6d34b9c7fb63a2c
simonittensammer <simon.ittensammer@gmx.at>
2021-05-06 14:56:37 +0200

    add POST endpoint for unit

--- timetable-backend/src/main/java/at/htl/boundary/SchoolclassResource.java
+++ timetable-backend/src/main/java/at/htl/boundary/SchoolclassResource.java
@@ -9,11 +9,13 @@ import org.jboss.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @ApplicationScoped
+@Path("/class")
 @Produces(MediaType.APPLICATION_JSON)
 public class SchoolclassResource {
 
--- timetable-backend/src/main/java/at/htl/boundary/UnitResource.java
+++ timetable-backend/src/main/java/at/htl/boundary/UnitResource.java
@@ -1,28 +1,61 @@
 package at.htl.boundary;
 
+import at.htl.control.SchoolclassRepository;
+import at.htl.control.TeacherRepository;
 import at.htl.control.UnitRepository;
 import at.htl.entity.Schoolclass;
+import at.htl.entity.Teacher;
 import at.htl.entity.Unit;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.json.JsonObject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 import java.util.List;
 
 @Path("/unit")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Transactional
 public class UnitResource {
 
     @Inject
     UnitRepository ur;
 
+    @Inject
+    TeacherRepository tr;
+
+    @Inject
+    SchoolclassRepository sr;
+
     @GET
     @Path("/class/{clazz}")
     public List<Unit> unitByClass(@PathParam("clazz") String clazz) {
         return ur.find("schoolclass_id", clazz).list();
+    }
+
+    @POST
+    public Response create(JsonObject jsonObject) {
+        Teacher teacher = tr.findById(
+                jsonObject.getJsonNumber("teacherId").longValue()
+        );
+
+        Schoolclass schoolclass = sr.find("id", jsonObject.getString("schoolclassId")).firstResult();
+
+        Unit unit = new Unit(
+                jsonObject.getJsonNumber("day").longValue(),
+                jsonObject.getJsonNumber("unit").longValue(),
+                jsonObject.getJsonString("subject").getString(),
+                teacher,
+                schoolclass
+        );
+
+        ur.persist(unit);
+        return Response.ok(unit).build();
     }
 }
