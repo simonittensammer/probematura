f16e3021979a4f8b788d696948244e989f9f37bc
simonittensammer <simon.ittensammer@gmx.at>
2021-05-06 13:16:14 +0200

    create quarkus project

---
+++ timetable-backend/src/test/java/at/htl/timetable/boundary/NativeDemoEndpointIT.java
@@ -0,0 +1,9 @@
+package at.htl.timetable.boundary;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeDemoEndpointIT extends DemoEndpointTest {
+
+    // Execute the same tests but in native mode.
+}
