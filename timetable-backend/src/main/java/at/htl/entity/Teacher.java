4d6349957b82de635efb037f81477641096ff0d5
simonittensammer <simon.ittensammer@gmx.at>
2021-05-06 13:44:33 +0200

    create entities

--- timetable-backend/src/main/java/at/htl/entity/Teacher.java
+++ timetable-backend/src/main/java/at/htl/entity/Teacher.java
@@ -1,4 +1,27 @@
 package at.htl.entity;
 
-public class Teacher {
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+
+@Entity
+@SequenceGenerator(name = "teacherSeq", sequenceName = "TEACHER_SEQ", initialValue = 1000)
+public class Teacher extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "teacherSeq")
+    public Long id;
+
+    public String firstName;
+    public String lastName;
+    public String room;
+
+    public Teacher() {
+    }
+
+    public Teacher(String firstName, String lastName, String room) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.room = room;
+    }
 }
